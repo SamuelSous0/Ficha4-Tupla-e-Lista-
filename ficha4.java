d5e5682a2d0694dd777a3c1ffb70d07d50029b1c
SamuelSous0 <samuel.00000859887@unicap.br>
2025-04-25 00:52:58 -0300

    Update ficha4.java

--- ficha4.java
+++ ficha4.java
@@ -1,8 +1,8 @@
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
-
-public class ficha4 {
+import java.util.HashMap;
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -53,6 +53,17 @@ public class ficha4 {
 //
 //        System.out.println(total);
 
+        // 05 Crie um dicionário com 3 pares de chave e valor representando nome e idade de pessoas. Mostre todas as idades.
+
+//        HashMap<String, Integer> pessoa = new HashMap<>();
+//        pessoa.put("Edmundo", 27);
+//        pessoa.put("Edmundo: o ultimo dobrador de ar", 15);
+//        pessoa.put("Kung FU Edmundo", 10);
+//
+//        for(String i : pessoa.keySet() ) {
+//            System.out.println(i + ": " + pessoa.get(i));
+//        }
+
         // 6 Peça 4 notas ao usuário e armazene em uma lista. Depois, calcule a média e exiba.
 
 //        double notas;
@@ -70,6 +81,33 @@ public class ficha4 {
 //        double media = total / notas1periodo.size();
 //
 //        System.out.println("Media: "+media);
+
+        // 07 Peça o nome de 3 produtos e seus respectivos preços e armazene em um dicionário. Mostre os produtos mais caros.
+
+//        HashMap<String, Double> produto  = new HashMap<>();
+//
+//        produto.put("Carta do tigrinho", 999.99);
+//        produto.put("Naipe", 15.0);
+//        produto.put("Puxe", 7.50);
+//
+//        double produtomaiscaro = 0;
+//        String chaveprodutomaiscaro = "";
+//
+//
+//
+//        for (String i : produto.keySet()) {
+//            if (produto.get(i) > produtomaiscaro) {
+//                produtomaiscaro = produto.get(i);
+//                chaveprodutomaiscaro = i;
+//
+//            }
+//        }
+//
+//        System.out.println(chaveprodutomaiscaro);
+
+
+
+
 
         // 08  Peça uma lista de 5 números e diga qual é o maior, o menor e a soma total.
 
@@ -109,6 +147,25 @@ public class ficha4 {
 //            }
 //        }
 
+        //  10 Crie um dicionário com 4 nomes e notas de alunos. Calcule e mostre a média geral da turma.
+
+//        HashMap<String, Double> aluno = new HashMap<>();
+//
+//        aluno.put("Edmundo", 10.0);
+//        aluno.put("Edzinh", 7.5);
+//        aluno.put("Edzão", 9.0);
+//        aluno.put("Ed", 4.0);
+//
+//        double total = 0;
+//
+//        for (Double i : aluno.values()) {
+//            total += i;
+//        }
+//
+//        double mediaturma = total/aluno.size();
+//
+//        System.out.println(mediaturma);
+
         // 11 Crie uma lista de 10 números inteiros e remova todos os pares.
 //            ArrayList<Integer> numerosImpares = new ArrayList<>();
 //            for (int i = 0; i < 10; i++) {
@@ -132,6 +189,22 @@ public class ficha4 {
 //            }
 //        }
 
+        // 12 Solicite ao usuário 3 pares de chave e valor e crie um dicionário a partir dessas entradas.
+
+//        HashMap<String, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < 3; i++) {
+//            System.out.print("Enter name " + (i+1) + ": ");
+//            String name = sc.nextLine();
+//            System.out.print("Enter number " + (i+1) + ": ");
+//            int number = sc.nextInt();
+//            sc.nextLine();
+//            map.put(name, number);
+//        }
+//
+//        for (String i : map.keySet()) {
+//            System.out.println(i+" "+map.get(i));
+//        }
 
         // 13 Crie uma tupla com os dias da semana e mostre apenas os dias úteis.
 
@@ -163,6 +236,30 @@ public class ficha4 {
 //            System.out.println("Lamento, seu nome não está na lista");
 //        }
 
+        // 16 Crie duas listas: uma com nomes e outra com idades. Junte essas listas em um dicionário
+
+//        ArrayList<String> nomes = new ArrayList<>();
+//        ArrayList<Integer> idade = new ArrayList<>();
+//        HashMap<String, Integer> map = new HashMap<>();
+//
+//        System.out.println("Digite a quantidade de alunos: ");
+//        int quantidade = sc.nextInt();
+//
+//        for (int i = 0; i < quantidade; i++) {
+//            System.out.println("Digite o nome do aluno "+(i+1)+": ");
+//            nomes.add(sc.next());
+//            System.out.println("Digite a idade do aluno "+(i+1)+": ");
+//            idade.add(sc.nextInt());
+//        }
+//
+//        for (int i = 0; i < nomes.size(); i++) {
+//            map.put(nomes.get(i), idade.get(i));
+//        }
+//
+//        for(String id : map.keySet()) {
+//            System.out.println(id + " - " + map.get(id) + " anos");
+//        }
+
         // 17 Peça ao usuário que digite nomes até que ele digite "sair". Mostre a lista ordenada dos nomes inseridos.
 
 //        ArrayList<String> nomes = new ArrayList<>();
@@ -183,6 +280,26 @@ public class ficha4 {
 //            System.out.println(nomes.get(i));
 //        }
 
+        // 18 Crie um dicionário com 5 cidades e suas temperaturas. Mostre a cidade mais quente.
+
+//        HashMap<String, Double> cidade = new HashMap<>();
+//        cidade.put("Curitiba", -10.0);
+//        cidade.put("Recife", 40.0);
+//        cidade.put("Xique-Xique", 15.0);
+//
+//        String cidademaisquente = "";
+//        ArrayList<Double> valores = new ArrayList<>();
+//
+//        for (String i : cidade.keySet()) {
+//            valores.add(cidade.get(i));
+//        }
+//
+//        Collections.sort(valores);
+//
+//        System.out.println(valores.getLast());
+
+
+
         // 19 Dada uma tupla com números, crie uma lista contendo apenas os valores maiores que 10.
 
 //        int[] numeros = {50,40,30,20,10,15,2,4,3,1,9};
@@ -198,6 +315,37 @@ public class ficha4 {
 //        for (int i = 0; i < lista.size(); i++) {
 //            System.out.println(lista.get(i));
 //        }
-        
+
+        // 20 Peça ao usuário que informe nomes e idades de pessoas. Armazene em um dicionário. Em seguida, exiba o nome da pessoa mais velha.
+
+//        HashMap<String, Integer> map = new HashMap<>();
+//
+//        System.out.println("Enter the number of elements in the map");
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; i++) {
+//            System.out.println("Enter the element " + i + ": ");
+//            String key = sc.next();
+//            System.out.println("Enter the value " + i + ": ");
+//            int value = sc.nextInt();
+//            sc.nextLine();
+//            map.put(key, value);
+//        }
+//
+//        String oldestName = "";
+//        int oldestAge = -1;
+//
+//
+//       for (String key : map.keySet()) {
+//            int age = map.get(key);
+//            if (age > oldestAge) {
+//                oldestAge = age;
+//                oldestName = key;
+//            }
+//       }
+//
+//        System.out.println("The oldest person is: " + oldestName + " (" + oldestAge + " years old)");
+
+
     }
 }
